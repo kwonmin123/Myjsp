9ea5957ff913d8886f91e353439892d59576df28
kwonmin123123 <zzacjk@naver.com>
2021-11-05 09:15:33 +0900

    Create Bean04.java

---
+++ src/sample03javabean/Bean04.java
@@ -0,0 +1,27 @@
+package sample03javabean;
+
+public class Bean04 {
+String nickName;
+int age;
+boolean vip;
+public String getNickName() {
+	return nickName;
+}
+public void setNickName(String nickName) {
+	this.nickName = nickName;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public boolean isVip() {
+	return vip;
+}
+public void setVip(boolean vip) {
+	this.vip = vip;
+}
+
+
+}
