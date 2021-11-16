c0e4ba4eab15c5dcf1103edbc30f6d5053cf6219
kwonmin123123 <zzacjk@naver.com>
2021-11-16 09:35:14 +0900

    Create S02AtrributeServlet.java

---
+++ src/servlet03/S02AtrributeServlet.java
@@ -0,0 +1,42 @@
+package servlet03;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S02AtrributeServlet
+ */
+@WebServlet("/servlet03/s02")
+public class S02AtrributeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public S02AtrributeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("food", "orange");
+		String path ="/WEB-INF/view/servlet03/s02.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
