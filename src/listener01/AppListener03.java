44e0e28d9e713962c99309a0c536da428d06cc5f
kwonmin123123 <zzacjk@naver.com>
2021-11-19 09:12:57 +0900

    Create AppListener03.java

---
+++ src/listener01/AppListener03.java
@@ -0,0 +1,37 @@
+package listener01;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class AppListener03
+ *
+ */
+//@WebListener
+public class AppListener03 implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public AppListener03() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("3번리스너 종료");
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("3번리스너 시작");
+    }
+	
+}
