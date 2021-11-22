0963c8f3836c92336328f3639e0bacfa97260aa1
kwonmin123123 <zzacjk@naver.com>
2021-11-22 18:16:36 +0900

    Create Customer2.java

---
+++ src/jdbc02/bean/Customer2.java
@@ -0,0 +1,53 @@
+package jdbc02.bean;
+
+public class Customer2 {
+private String customerID;
+private String customerName;
+private String contactName;
+private String address;
+public String getCustomerID() {
+	return customerID;
+}
+public void setCustomerID(String customerID) {
+	this.customerID = customerID;
+}
+public String getCustomerName() {
+	return customerName;
+}
+public void setCustomerName(String customerName) {
+	this.customerName = customerName;
+}
+public String getContactName() {
+	return contactName;
+}
+public void setContactName(String contactName) {
+	this.contactName = contactName;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getPostalCode() {
+	return postalCode;
+}
+public void setPostalCode(String postalCode) {
+	this.postalCode = postalCode;
+}
+public String getCountry() {
+	return country;
+}
+public void setCountry(String country) {
+	this.country = country;
+}
+private String city;
+private String postalCode;
+private String country;
+}
