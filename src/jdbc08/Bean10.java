93d2be7cba58870918d74dd34c44c78f02ada466
kwonmin123123 <zzacjk@naver.com>
2021-11-27 09:23:35 +0900

    Create Bean10.java

---
+++ src/jdbc08/Bean10.java
@@ -0,0 +1,58 @@
+package jdbc08;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Bean10 {
+	private int col1;
+	private double col2;
+	private double col3;
+	private String col4;
+	private String col5;
+	private LocalDate col6;
+	private LocalDateTime col7;
+	public int getCol1() {
+		return col1;
+	}
+	public void setCol1(int col1) {
+		this.col1 = col1;
+	}
+	public double getCol2() {
+		return col2;
+	}
+	public void setCol2(double col2) {
+		this.col2 = col2;
+	}
+	public double getCol3() {
+		return col3;
+	}
+	public void setCol3(double col3) {
+		this.col3 = col3;
+	}
+	public String getCol4() {
+		return col4;
+	}
+	public void setCol4(String col4) {
+		this.col4 = col4;
+	}
+	public String getCol5() {
+		return col5;
+	}
+	public void setCol5(String col5) {
+		this.col5 = col5;
+	}
+	public LocalDate getCol6() {
+		return col6;
+	}
+	public void setCol6(LocalDate col6) {
+		this.col6 = col6;
+	}
+	public LocalDateTime getCol7() {
+		return col7;
+	}
+	public void setCol7(LocalDateTime localDateTime) {
+		this.col7 = localDateTime;
+	}
+	
+}
