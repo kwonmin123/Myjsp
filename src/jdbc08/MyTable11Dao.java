7b80efc8997acf23bc86a201a5fe83ec7fbd6e0f
kwonmin123123 <zzacjk@naver.com>
2021-11-27 09:24:27 +0900

    Create MyTable11Dao.java

---
+++ src/jdbc08/MyTable11Dao.java
@@ -0,0 +1,13 @@
+package jdbc08;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class MyTable11Dao {
+
+	public List<Bean11> getList(Connection con) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
